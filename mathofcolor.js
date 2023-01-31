db4ca7352682ed96efab8c5890e5372dd1ac89fa
Everett <heyoitses@gmail.com>
2023-01-31 13:53:15 -0600

    homeworks test 1

---
+++ mathofcolor.js
@@ -0,0 +1,27 @@
+
+    
+    
+    function Complimentary (){
+    let r;
+    let b;
+    let g;
+
+    let cr;
+    let cb;
+    let cg;
+    
+        r = document.getElementById("r").value; 
+        b = document.getElementById("b").value; 
+        g = document.getElementById("g").value;
+
+        cr = 255 - r;
+        cb = 255 - b;
+        cg = 255 - g;
+
+        result = ("The complimentary color of " + r +", " + b + ", " +
+g + " is " + cr + ", " + cb + ", " + cg) 
+
+document.getElementById("result").innerHTML = result;
+
+}
+document.getElementById("submit").addEventListener("click", Complimentary);
