b6aa524eae81acacd2db7447d6c03e1e5f1c098e
Everett <heyoitses@gmail.com>
2023-02-26 12:19:37 -0600

    math of color exceptions

--- clientsidenav_projects/color_theory_website/mathofcolor.js
+++ clientsidenav_projects/color_theory_website/mathofcolor.js
@@ -1,8 +1,13 @@
 
-    
-    
-    function Complimentary (){
+        const MIN = 0;
+        const MAX = 255;
 
+        warningBox = document.getElementById("warningBox");
+        warningBox.innerHTML = "";
+    
+        document.getElementById("submit").addEventListener("click", Complimentary);
+
+    function Complimentary (){
     let r;
     let b;
     let g;
@@ -11,17 +16,35 @@
     let cb;
     let cg;
     
-        r = document.getElementById("r").value; 
-        b = document.getElementById("b").value; 
-        g = document.getElementById("g").value;
+    r = document.getElementById("r").value; 
+    b = document.getElementById("b").value; 
+    g = document.getElementById("g").value;
 
+    try{
+        if(r > MAX || r<MIN) throw "Must be a number between 0-255.";
+        if(b > MAX || b<MIN) throw "Must be a number between 0-255.";
+        if(g > MAX || g<MIN) throw "Must be a number between 0-255.";
+
+        if(r == "") throw "Must be a number between 0-255.";
+        if(b =="") throw "Must be a number between 0-255.";
+        if(g =="") throw "Must be a number between 0-255.";
+
+        
         cr = 255 - r;
         cb = 255 - b;
         cg = 255 - g;
-
+        
         result = ("The complimentary color of " + r +", " + b + ", " +
-g + " is " + cr + ", " + cb + ", " + cg) 
-
+        g + " is " + cr + ", " + cb + ", " + cg)
+ 
         window.alert(result)
+     }
+     catch(err){
+        warningBox.innerHTML=" " + err;
+        
+     }
+
+        
 }
-document.getElementById("submit").addEventListener("click", Complimentary);
+
+
