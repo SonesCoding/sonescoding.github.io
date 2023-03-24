9a350694920d8e49263ef5bf36d9d33c59ab2660
Everett <heyoitses@gmail.com>
2023-03-24 11:16:46 -0500

    6-1

--- clientsidenav_projects/mod6/project01/Sonesproject06-01.js
+++ clientsidenav_projects/mod6/project01/Sonesproject06-01.js
@@ -4,9 +4,25 @@
       Project 06-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Lillian S
+      Date:   3/22/23
 
       Filename: project06-01.js
 */
 
+let submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function (){
+
+      if(pwd.validity.patternMismatch) {
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+      }
+      else if(pwd.value != pwd2.value){
+            pwd.setCustomValidity("Your passwords must match");
+      }
+      else{
+            pwd.setCustomValidity("");
+      }
+});
